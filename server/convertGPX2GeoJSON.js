749af9ced6df8cc0bda82c9664753d40122459be
BlackGanglion <BlackGanglion@blackganglion.com>
2025-03-26 13:50:09 +0800

    feat: add outdoor.json

--- server/convertGPX2GeoJSON.js
+++ server/convertGPX2GeoJSON.js
@@ -4,14 +4,14 @@ import xmldom from 'xmldom';
 
 const DOMParser = xmldom.DOMParser;
 
-const gpx = new DOMParser().parseFromString(fs.readFileSync('../route/TUOLUMNEMEADOWSOutdoorClimb20240427081229.gpx', 'utf8'));
+const gpx = new DOMParser().parseFromString(fs.readFileSync('../route/450092046183596032.gpx', 'utf8'));
 
 const geojson = tj.gpx(gpx);
 
 const data = JSON.stringify(geojson);
 
 // write JSON string to a file
-fs.writeFile('../route/TUOLUMNEMEADOWSOutdoorClimb20240427081229.json', data, (err) => {
+fs.writeFile('../route/450092046183596032.json', data, (err) => {
   if (err) {
     throw err;
   }
